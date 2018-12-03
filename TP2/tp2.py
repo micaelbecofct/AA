330f1ef868cf9c815264cc724dba72a08c8cddf4
VisualStellar2 <33594267+VisualStellar2@users.noreply.github.com>
2018-12-03 19:26:18 +0000

    Ja sei obter ponto depois do kneighbors

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -29,73 +29,96 @@ from sklearn import cluster as cl
 from sklearn import mixture as mx
 from sklearn import neighbors as nb
 from plotClasses import plot_classes
+RADIUS = 6371 #raio da terra em km
 
-RADIUS = 6371  # raio da terra em km
+def lat_lon_to_3d(lat,lon):
+	x = RADIUS * np.cos(lat * np.pi / 180) * np.cos(lon * np.pi / 180)
+	y = RADIUS * np.cos(lat * np.pi / 180) * np.cos(lon * np.pi / 180)
+	z = RADIUS * np.sin(lat * np.pi / 180)
+	return x,y,z
 
-
-def lat_lon_to_3d(lat, lon):
-    x = RADIUS * np.cos(lat * np.pi / 180) * np.cos(lon * np.pi / 180)
-    y = RADIUS * np.cos(lat * np.pi / 180) * np.cos(lon * np.pi / 180)
-    z = RADIUS * np.sin(lat * np.pi / 180)
-    return x, y, z
-
-
-# guardar latitude, longitude e falha de cada sismo
+#guardar latitude, longitude e falha de cada sismo
 def get_data(filename):
-    data = pd.read_csv(filename)
-    faults = data["fault"]
-    latitudes = data["latitude"]
-    longitudes = data["longitude"]
-    return faults, latitudes, longitudes
+	data = pd.read_csv(filename)
+	faults = data["fault"]
+	latitudes = data["latitude"]
+	longitudes = data["longitude"]
+	return faults, latitudes, longitudes
 
-
-# devolve array com (x,y,z) de cada sismo
+#devolve array com (x,y,z) de cada sismo
 def all_points_to_3d(latitudes, longitudes):
-    points = []
-    for ix in range(len(latitudes)):
-        x, y, z = lat_lon_to_3d(latitudes[ix], longitudes[ix])
-        points.append([x, y, z])
-    return points
+	points = []
+	for ix in range(len(latitudes)):
+		x,y,z = lat_lon_to_3d(latitudes[ix],longitudes[ix])
+		points.append([x,y,z])
+	return points
 
-
-# devolve array com clusterings para cada num de clusters
+#devolve array com clusterings para cada num de clusters
 def k_means_cluster(points):
-    clustering_labels = [];
-    print("started clustering with k-means")
-    for ix in range(80, 100):  # ha 90 falhas
-        clustering = cl.KMeans(n_clusters=ix).fit(points)
-        print(((ix + 1) - 80), "/20 clusterings, ", clustering.n_iter_, "iterations this time")
-        clustering_labels.append(clustering.labels_)
-    return clustering_labels
+	clustering_labels = [];
+	print("started clustering with k-means")
+	for ix in range(80,100):#ha 90 falhas
+		clustering = cl.KMeans(n_clusters = ix).fit(points)
+		print(((ix + 1) - 80),"/20 clusterings, ",clustering.n_iter_,"iterations this time")
+		clustering_labels.append(clustering.labels_)
+	return clustering_labels
 
-
-# devolve array com clusterings para cada num de componentes gaussianos
+#devolve array com clusterings para cada num de componentes gaussianos
 def gauss_mm(points):
-    clustering_labels = [];
-    print("started clustering with Gaussian Mixture Models")
-    for ix in range(80, 100):  # ha 90 falhas
-        clustering = mx.GaussianMixture(n_components=ix).fit(points)
-        clustering_labels.append(clustering.predict(points))
-        print(((ix + 1) - 80), "/20 clusterings, ", clustering.n_iter_, "iterations this time")
-    return clustering_labels
-
+	clustering_labels = [];
+	print("started clustering with Gaussian Mixture Models")
+	for ix in range(80,100):#ha 90 falhas
+		clustering = mx.GaussianMixture(n_components = ix).fit(points)
+		clustering_labels.append(clustering.predict(points))
+		print(((ix + 1) - 80),"/20 clusterings, ",clustering.n_iter_,"iterations this time")
+	return clustering_labels
 
 def plot_for_dbscan(points):
     dists = []
     knn = nb.KNeighborsClassifier(n_neighbors=4)
     knn.fit(points, np.zeros(len(points)))
     for ix in range(len(points)):
-        curr = knn.kneighbors([points[ix]], n_neighbors=4)[0]
-        dists.append(curr)
-
-
+        curr = knn.kneighbors([points[ix]],n_neighbors=4)[0]
+        dists.append(curr.item(3))#o kneighbors devolve o array ordenado, a dist mais alta e a ultima
+    dists.sort(); dists.reverse()
+    return dists
+    
 faults, latitudes, longitudes = get_data("tp2_data.csv")
 points = all_points_to_3d(latitudes, longitudes)
-print("points[5]: ", points[5], "\n")
-# kmeans = k_means_cluster(points)
-# print("labels[9]: ",labels[9])
-# plot_classes(kmeans[0],longitudes,latitudes)
-# gaussian = gauss_mm(points)
-# print("gaussian[0]: ",gaussian[0])
-# plot_classes(gaussian[0],longitudes,latitudes)
-plot_for_dbscan(points)
+print("points[5]: ",points[5],"\n")
+#kmeans = k_means_cluster(points)
+#print("labels[9]: ",labels[9])
+#plot_classes(kmeans[0],longitudes,latitudes)
+#gaussian = gauss_mm(points)
+#print("gaussian[0]: ",gaussian[0])
+#plot_classes(gaussian[0],longitudes,latitudes)
+dists = plot_for_dbscan(points)
+print(dists)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
